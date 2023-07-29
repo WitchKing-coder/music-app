a90ca884c9210714403fc69e3a8778f04487b043
WitchKing-coder <Sg068639@gmail.com>
2023-07-29 23:34:10 +0300

    Added base functions to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import snailIcon from '../../assets/images/snailIcon.svg';
 import profileIcon from '../../assets/images/profile.svg'
 import './header.scss'
+import {useNavigate} from "react-router";
 
 const Header = () => {
+    const navigate = useNavigate()
+    const [isOpenSettings, setIsOpenSettings] = useState<boolean>(false)
+
     return (
         <div className="header_Container">
-            <img src={snailIcon} alt=""/>
+            <img onClick={() => navigate("/home")} src={snailIcon} alt=""/>
             <input type="text" placeholder="🔍︎"/>
-            <img src={profileIcon} alt=""/>
+            <img onClick={() => setIsOpenSettings(!isOpenSettings)} src={profileIcon} alt=""/>
         </div>
     );
 };
